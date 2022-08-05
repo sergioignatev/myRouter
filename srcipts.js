ea026abb5d215e3759ff8e3a45cc45d3f79a1000
sergioIgnatev <sergioignatev17@gmail.com>
2022-08-05 22:29:37 +0300

    #

--- srcipts.js
+++ srcipts.js
@@ -1,13 +1,14 @@
 let dem=`<h1 id="demo">DEMO</h1>`
 let abou =`<h1 id="about">About</h1>`
 let id=document.getElementById("content")
+document.getElementById("demobtn").addEventListener("click",demo)
 function demo(){
     id.innerHTML=dem
-    history.forward()
+   
 
 }
 demo()
 function about(){
     id.innerHTML=abou;
-    history.forward()
+    
 }
