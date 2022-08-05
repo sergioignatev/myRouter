288d9c5cfb77699b55e94553be18a1c6191535a9
sergioIgnatev <sergioignatev17@gmail.com>
2022-08-05 21:41:45 +0300

    ddd

---
+++ srcipts.js
@@ -0,0 +1,10 @@
+let dem=`<h1 id="demo">DEMO</h1>`
+let abou =`<h1 id="about">About</h1>`
+let id=document.getElementById("content")
+function demo(){
+    id.innerHTML=dem
+}
+demo()
+function about(){
+    id.innerHTML=abou
+}
